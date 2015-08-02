4e9e2d2ea18e0fcd9e475627616fa21bc21fcc81
Desingh <desinghrajan@brocode-inc.com>
2015-08-02 13:40:15 +0530

    Adding service for github http communications

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 var githubUsers = angular.module('githubUsers', []);
 
-var MainController = function ($scope, $http) {
+var MainController = function ($scope, github) {
   $scope.message = 'Search Github Users!';
+  $scope.reposSortOrder = '-stargazers_count';
 
   $scope.search = function (username) {
-    $http.get('https://api.github.com/users/' + username).
-        success(function (data) {
-          $scope.person = data;
-        }).
-        error(function (data) {
-          $scope.error = 'Could not get user details'
-        })
+    github.user(username).
+        then(function (data) {
+          $scope.user = data;
+          github.repos($scope.user).
+              then(function (data) {
+                $scope.repos = data
+              }, function (data) {
+                $scope.error = 'Could not fetch repos'
+              });
+        }, function (data) {
+          $scope.error = 'Could not fetch user details'
+        });
   };
 };
-
-githubUsers.controller('MainController', ['$scope', '$http', MainController]);
+githubUsers.controller('MainController', MainController);
---
+++ services/github.js
@@ -0,0 +1,21 @@
+var github = function ($http) {
+  var getUser = function (username) {
+    return $http.get('https://api.github.com/users/' + username).
+        then(function(response){
+          return response.data;
+        });
+  };
+
+  var getRepos = function (user) {
+    return $http.get(user.repos_url).
+        then(function(response){
+          return response.data
+        });
+  };
+
+  return {
+    user: getUser,
+    repos: getRepos
+  }
+};
+githubUsers.factory('github', github);
